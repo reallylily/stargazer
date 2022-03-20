9f095126262bc7c9a9d72938d38b2b469a0d4a8c
Lily Reale <reallylilyreale@gmail.com>
2022-03-19 22:25:01 -0700

    Prettier works

--- src/components/_app/App/App.tsx
+++ src/components/_app/App/App.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 import ApiProvider from 'components/_app/ApiProvider/ApiProvider';
@@ -6,22 +5,14 @@ import ErrorBoundary from 'components/_app/ErrorBoundary/ErrorBoundary';
 
 // import styles from './App.module.scss';
 
-
-
 export const App: React.FC = () => {
-
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to heeeeeeeere.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
