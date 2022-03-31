7ff13d4a6393ff1210a2c490d0e2977a61f0d22c
Lily Reale <reallylilyreale@gmail.com>
2022-03-30 20:21:21 -0700

    cleanup

--- src/tests/server.ts
+++ src/tests/server.ts
@@ -1,11 +1,4 @@
-// import { setupWorker } from 'msw';
-
-// import { PlatformConfig } from 'config';
 import { setupServer } from 'msw/node';
 import handlers from 'tests/handlers';
 
-// export const server = setupWorker(...handlers);
 export const server = setupServer(...handlers);
-// export const server = PlatformConfig.isTestRun ? setupWorker(...handlers) : setupServer(...handlers);
-
-// export const server = setupServer(...handlers);
